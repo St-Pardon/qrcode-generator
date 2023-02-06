21227de0473e387fd41006fdcbc0d5bd5f17c053
St Pardon <okc4pardon@gmail.com>
2023-02-06 20:18:23 +0100

    :hammer: completed the backend of the application

---
+++ src/controllers/generate.controller.js
@@ -0,0 +1,16 @@
+const {toDataURL} = require('qrcode')
+
+
+const generate = (req, res) => {
+  const { url } = req.body;
+
+  // check if url is provided
+  if (!url) res.send('No URL was provided');
+  toDataURL(url, (err, src) => {
+    if (err) res.send('An error occured try again later');
+    console.log(typeof src )
+    res.render('generate', { src });
+  });
+};
+
+module.exports = generate;
